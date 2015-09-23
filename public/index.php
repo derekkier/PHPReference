d1b94686583a174ad45282ef736a3ff5fb1798d9
derekkier <derek.kier@gmail.com>
2015-09-23 01:59:23 -0700

    Connect to sqlite3 database

--- public/index.php
+++ public/index.php
@@ -1,10 +1,26 @@
 <html>
 	<head>
-		<title>PHP Reference</title>
+		<title>PHP SQLite3</title>
 	</head>
 	<body>
-	<?php 
+<?php
 
+$db = new PDO('sqlite:../sqlite3/estimotesManager.db');
+$result = $db->query("select * from users");
+foreach( $result as $row)
+{
+	print_r($row);
+}
+
+$db->query("update users set password='temp' where username='derekkier'");
+
+phpinfo();
+die();
+if ($db = new SQLiteDatabase('../sqlite3/test.db')) 
+{
+	echo 'sqlite exists';
+			//$q = @$db->query('SELECT requests FROM tablename WHERE id = 1');
+}
 	?>
 	</body>
 </html>
